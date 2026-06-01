8328b34c53f2b38a549f4ad33ec386a74033eb4d
vasilikikonou <v.konou02@gmail.com>
2026-06-01 11:00:58 +0300

    changed name and color right top

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def squares():
     # 1. Define the list of fixed hex color codes
     colors = [
-        "#FF5733", "#33FF57", "#3357FF", "#F1C40F", "#8E44AD",
+        "#FF5733", "#33FF57", "#3357FF", "#F1C40F", "#000000",
         "#E67E22", "#2ECC71", "#3498DB", "#E74C3C", "#95A5A6",
         "#9B59B6", "#34495E", "#16A085", "#E67E22", "#27AE60",
         "#95A5A6", "#D35400", "#C0392B", "#7F8C8D", "#F39C12",
